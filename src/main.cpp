a5ab0735511037911aa98d136841f4e7846a1a2b
chiyizipi <3045059088@qq.com>
2022-12-02 19:47:10 +0800

    Co-authored-by: chiyizipi <3045059088@qq.com>

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
+int main()
+{
+    for (int i = 0; i < 30; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            // int f = 0;
+            // if (i % 2 == 0)
+            //     f++;
+            // if (j % 2 == 0)
+            //     f++;
+            // if (f == 2)
+            //     std::cout << '+';
+            // else if (f == 0)
+            //     std::cout << '*';
+            // else if (i % 2 == 0)
+            //     std::cout << '-';
+            // else
+            //     std::cout << "|";
+            // std::cout << "草";
+            std::cout << RED << "*" << RESET;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
