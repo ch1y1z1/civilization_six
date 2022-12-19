d721f264fd7ad8a74987a7b95d880138c2d45f43
chiyizipi <3045059088@qq.com>
2022-12-19 13:34:47 +0800

    kkk

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Display.h"
 #include <cstring>
+#include <vector>
 using namespace std;
 
 int main()
@@ -11,7 +12,7 @@ int main()
     Activity* exhibition = new Activity((char*)"exhibition", 50, Attributes(0, 30.0));
     Activity* distribution = new Activity((char*)"distribution", 50, Attributes(0, 0, 0, 30.0));
 
-    Activity** activities = new Activity*[3]{symposium, exhibition, distribution};
+    Activity** activities = new Activity * [3] {symposium, exhibition, distribution};
 
     Building* center = new Building((char*)"cen", 0.0, Attributes(0.0), Attributes(0.7, 0.3, 0.5, 0.5), Attributes(1.0), (char*)"a", NOTYPE, symposium);
     Building* campus = new Building((char*)"cam", 100.0, Attributes(1.0), Attributes(0.2), Attributes(1.0), (char*)"a", MOUNTAIN, symposium);
@@ -19,13 +20,13 @@ int main()
     Building* wonder = new Building((char*)"won", 250.0, Attributes(0, 20.0), Attributes(0), Attributes(5.0), (char*)"w", NOTYPE);
     Building* industrialPark = new Building((char*)"ind", 100.0, Attributes(0, 0, 0, 2.0), Attributes(0, 0, 0, 2.0), Attributes(1.0), (char*)"a", NOTYPE, distribution);
 
-    Building** buildings = new Building*[5]{center, campus, theatre, wonder, industrialPark};
+    Building** buildings = new Building * [5] {center, campus, theatre, wonder, industrialPark};
 
-    Grid *grid = new Grid("./resource/map.txt");
+    Grid* grid = new Grid("./resource/map.txt");
 
     Display d;
     d.MainProcess(controller, grid, buildings, activities);
 
     return 0;
-    
+
 }
--- src/Controller.cpp
+++ src/Controller.cpp
@@ -15,7 +15,7 @@ Controller::~Controller()
     //dtor
 }
 
-void Controller::reset(int pop, float firstBorderThreshold){
+void Controller::reset(int pop, float firstBorderThreshold) {
     this->currentRound = 0;
     this->world = 0;
     this->pop = pop;
@@ -23,67 +23,71 @@ void Controller::reset(int pop, float firstBorderThreshold){
     this->currentAttributes = Attributes(0);
 }
 
-bool Controller::checkWin(){
+bool Controller::checkWin() {
     // todo: check whether the player has won the game
+    if (this->currentAttributes.tech >= 500) return true;
+    if (this->currentAttributes.cul >= 350) return true;  // TODO: add 5 qiguan
+    int score = 0.5 * this->currentAttributes.tech + 0.5 * this->currentAttributes.cul + 10 * this->pop + this->currentAttributes.prod;
+    if (score >= 500) return true;
     return false;
 }
 
-bool Controller::bindGrid(Grid* grid){
+bool Controller::bindGrid(Grid* grid) {
     this->world = grid;
     return (grid != 0);
 }
 
-Attributes Controller::getAttributes(){
+Attributes Controller::getAttributes() {
     return this->currentAttributes;
 }
 
-int Controller::getPop(){
+int Controller::getPop() {
     return this->pop;
 }
 
-int Controller::getWorkingPop(int &workersNumber, int *&workersCellCoords){
+int Controller::getWorkingPop(int& workersNumber, int*& workersCellCoords) {
     workersNumber = this->workingPop;
-    if(workersCellCoords != 0) delete [] workersCellCoords;
-    if(workersNumber == 0){
+    if (workersCellCoords != 0) delete[] workersCellCoords;
+    if (workersNumber == 0) {
         workersCellCoords = 0;
         return 0;
     }
-    workersCellCoords = new int[2 * workersNumber]{0};
+    workersCellCoords = new int[2 * workersNumber] {0};
     Cell** grid = this->world->getGrid();
     int m = this->world->getGridHeight(), n = this->world->getGridWidth();
     int cur = 0;
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(grid[i][j].Pop == COLONIZED){
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j].Pop == COLONIZED) {
                 workersCellCoords[cur] = i;
                 workersCellCoords[cur + 1] = j;
                 cur += 2;
             }
         }
     }
-    if(cur != workersNumber * 2) return -1;
+    if (cur != workersNumber * 2) return -1;
     return 0;
 }
 
-Cell Controller::getCellDescription(int m, int n){
+Cell Controller::getCellDescription(int m, int n) {
     return world->getRepresent(m, n);
 }
 
-int Controller::getRound(){
+int Controller::getRound() {
     return currentRound;
 }
 
-bool Controller::SetProductionBuilding(int m, int n, Building* building){
+bool Controller::SetProductionBuilding(int m, int n, Building* building) {
     // todo: set the current production to building at coordinates (m, n), return true if the action is successful
 
 }
 
-bool Controller::SetProductionActivity(Activity* activity){
+bool Controller::SetProductionActivity(Activity* activity) {
     // todo: set the current production to activity, return true if the action is successful
 }
 
-bool Controller::SetProductionActivity(int activityOrder){
-    if(activityOrder >= activitySize || activityOrder < 0)
+bool Controller::SetProductionActivity(int activityOrder) {
+    if (activityOrder >= activitySize || activityOrder < 0)
         return false;
     currentProductionType = PRODUCTION_TYPE_ACTIVITY;
     currentProductionCell = 0;
@@ -91,66 +95,85 @@ bool Controller::SetProductionActivity(int activityOrder){
     return true;
 }
 
-bool Controller::nextRound(int &newX, int &newY, float &nextThres){
+bool Controller::nextRound(int& newX, int& newY, float& nextThres) {
     if (this->checkWin())
         return 1;
     std::cin >> newX >> newY;
 
+    this->currentRound++;
+    this->updatePop();
+    this->updateProduction();
+    this->updateAttributes();
+
+    nextThres = this->BorderExpandThreshold;
+    int borderUpdate = this->checkBorderUpdate(newX, newY);
+    if (borderUpdate == 1) {
+        this->BorderExpandThreshold = 0.5;
+    }
+    else if (borderUpdate == 0) {
+        this->BorderExpandThreshold += 0.1;
+    }
+    else {
+        this->BorderExpandThreshold = 0.5;
+    }
+
+
+
     return 0;
 }
 
-int Controller::updateProduction(){
+int Controller::updateProduction() {
     // todo: update the process of current prodution
 }
 
-Production* Controller::getProduction(int &productionType, Cell *&currentProductionCell){
+Production* Controller::getProduction(int& productionType, Cell*& currentProductionCell) {
     productionType = this->currentProductionType;
     currentProductionCell = this->currentProductionCell;
     return this->currentProduction;
 }
 
 
-int Controller::checkPop(){
-    if(int(this->workingPop) > this->pop)
+int Controller::checkPop() {
+    if (int(this->workingPop) > this->pop)
         return -1;  //you have to remove pops
-    else if(int(this->workingPop) < this->pop)
+    else if (int(this->workingPop) < this->pop)
         return 1;   //you may add pops to work
     else return 0;  //you may switch pops
 }
 
-void Controller::updatePop(){
+void Controller::updatePop() {
     int maxpop = this->currentAttributes.food / 2;
     float popDelta = (maxpop - pop) * 0.1;
-    if(popDelta > 1)
+    if (popDelta > 1)
         popDelta = 1;
-    else if(popDelta < -1)
+    else if (popDelta < -1)
         popDelta = -1;
     this->pop += popDelta;
 }
 
-int Controller::checkBorderUpdate(int &newX, int &newY){
+int Controller::checkBorderUpdate(int& newX, int& newY) {
     // todo: check whether the border should update or not, and if so, return the new culture threshold and expansion coordinates (newX, newY)
 }
 
-int Controller::setPopAt(int m, int n, bool isAdding){
+int Controller::setPopAt(int m, int n, bool isAdding) {
     // todo: add or remove the worker at (m, n), return 0 if the action is successful
 }
 
-void Controller::updateAttributes(){
+void Controller::updateAttributes() {
     // todo: calculate and update the attributes according to the buffs
 }
 
-Cell** Controller::getAdjacentCells(int m, int n){
+Cell** Controller::getAdjacentCells(int m, int n) {
     // todo: get the adjacent cells from cell (m, n)
 }
 
-int Controller::getAdjacentSatisfied(Cell** adjacents, char buildingName, Landform landformType){
+int Controller::getAdjacentSatisfied(Cell** adjacents, char buildingName, Landform landformType) {
     // todo: check whether the adjacent cells satisfy the landformType and buildingName(s), you need to rewrite it if you use a hexagonal map
     unsigned satisfiedCellsNum = 0;
-    for(int t = 0; t < 4; t++){
-        if(adjacents[t] != 0){
+    for (int t = 0; t < 4; t++) {
+        if (adjacents[t] != 0) {
             satisfiedCellsNum += ((adjacents[t]->buildingType && (buildingName == 'a' || adjacents[t]->buildingType->name[0] == buildingName))
-                                  && adjacents[t]->landform == landformType);
+                && adjacents[t]->landform == landformType);
         }
     }
     return satisfiedCellsNum;
