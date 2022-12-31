632400677719ce8e6123a4f4e7d873618e86a187
chiyizipi <3045059088@qq.com>
2022-12-31 20:05:21 +0800

    hfgh

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -191,7 +191,7 @@ int Controller::updateProduction()
                     std::cout << "invalid input: out of range" << std::endl;
                     continue;
                 }
-                // this->currentProductionType = PRODUCTION_TYPE_BUILDING;
+                this->currentProductionType = PRODUCTION_TYPE_BUILDING;
                 this->currentProductionCell = &this->getCellDescription(m, n);
                 this->currentProduction = this->availableBuildings[buildingchoice];
                 this->prod_needed_to_active = this->availableBuildings[buildingchoice]->prodSpent * this->getRound() / 100;
