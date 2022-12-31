61c160e7fec41100c907be03cd41c3bfbc8e1a7a
chiyizipi <3045059088@qq.com>
2022-12-31 23:48:28 +0800

    bcvxbcxv

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -461,11 +461,11 @@ void Controller::updateAttributes()
             if (this->getCellDescription(i, j).Pop == OWNED && this->getCellDescription(i, j).buildingType == 0)
             {
                 this->currentAttributes.food += 0.5 * landformBuffs[this->getCellDescription(i, j).landform][0];
-                this->currentAttributes.prod += 0.5 * landformBuffs[this->getCellDescription(i, j).landform][1];
+                this->currentAttributes.prod += 0.25 * landformBuffs[this->getCellDescription(i, j).landform][1];
             }
             if (this->getCellDescription(i, j).buildingType != 0)
             {
-                this->currentAttributes +=  this->getCellDescription(i, j).buildingType->basicBonus * 0.5 ;
+                this->currentAttributes += this->getCellDescription(i, j).buildingType->basicBonus * 0.5;
             }
         }
     }
