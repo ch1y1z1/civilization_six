e7a4d80651a4ec24963efc5ab8ee7bc4efa2334c
megdeerer <1693408278@qq.com>
2023-01-01 00:18:33 +0800

    1

--- src/Display.cpp
+++ src/Display.cpp
@@ -128,7 +128,7 @@ int Display::DrawAttributes(Controller* controller, Grid* grid)
     go(65, 26);
     cout << "production:" << controller->getAttributes().prod;
     go(65, 27);
-    cout << "score:" << controller->getAttributes().prod + 10 * pop + controller->getAttributes().cul / 2.0 + controller->getAttributes().tech / 2.0;
+    cout << "score:" << (controller->getAttributes().prod-1) + 10 * pop + controller->getAttributes().cul / 2.0 + controller->getAttributes().tech / 2.0;
     return pop;
 }
 
