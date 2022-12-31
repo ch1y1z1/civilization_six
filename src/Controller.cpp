1525ba7e4f3ca796990eafcf3c1417adb99b8e8e
chiyizipi <3045059088@qq.com>
2022-12-31 22:59:31 +0800

    gfdsgdfs

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -207,7 +207,8 @@ int Controller::updateProduction()
                 this->currentProductionCell = &this->getCellDescription(m, n);
                 this->currentProduction = this->availableBuildings[buildingchoice];
                 this->prod_needed_to_active = this->availableBuildings[buildingchoice]->prodSpent * this->getRound() / 100;
-                this->round_needed_to_active = this->prod_needed_to_active / this->currentAttributes.prod;
+                this->round_needed_to_active = this->prod_needed_to_active / this->currentAttributes.prod + 1;
+                this->totle_round_needed = this->round_needed_to_active;
                 this->getCellDescription(m, n).buildingType = this->availableBuildings[buildingchoice];
                 this->getCellDescription(m, n).IF_BUILDING = 1;
 
