46ac90d1082538ddf131009e7e471f133489eafc
chiyizipi <3045059088@qq.com>
2023-01-01 20:48:51 +0800

    dsfadsf

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -608,12 +608,12 @@ void Controller::updateAttributes()
         {
             if (this->getCellDescription(i, j).Pop == COLONIZED && this->getCellDescription(i, j).buildingType == 0)
             {
-                this->currentAttributes.food += 0.5 * landformBuffs[this->getCellDescription(i, j).landform][0];
-                this->currentAttributes.prod += 0.25 * landformBuffs[this->getCellDescription(i, j).landform][1];
+                this->currentAttributes.food += 1 * landformBuffs[this->getCellDescription(i, j).landform][0];
+                this->currentAttributes.prod += 0.5 * landformBuffs[this->getCellDescription(i, j).landform][1];
             }
-            if (this->getCellDescription(i, j).buildingType != 0 && this->getCellDescription(i, j).IF_BUILDING == 0)
+            if (this->getCellDescription(i, j).buildingType != 0 && this->getCellDescription(i, j).IF_BUILDING == 0 && this->getCellDescription(i, j).Pop == COLONIZED)
             {
-                this->currentAttributes += this->getCellDescription(i, j).buildingType->basicBonus * 0.5;
+                this->currentAttributes += this->getCellDescription(i, j).buildingType->basicBonus * 1;
             }
         }
     }
